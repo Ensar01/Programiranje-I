b3433e23f1468eff97a6a61039d9c409e6e7342d
Ensar Mulahmetović <ensar.mulahmetovic@edu.fit.ba>
2021-08-31 01:31:07 +0200

    Update 09.cpp

--- Zadatak III/09.cpp
+++ Zadatak III/09.cpp
@@ -1,1 +1,30 @@
+//Radi do 1023 sve povis je izvan range-a int-a
 
+#include<iostream>
+using namespace std;
+
+int konverzijaBinarni(int);
+
+int main()
+{
+    int x;
+    cout << "Unesi neki broj koji zelite pretvoriti u binarni:" << endl;
+    cin >> x;
+
+    cout << x << " u binarnom sistemu iznosi: " << konverzijaBinarni(x);
+
+    cin.get();
+    return 0;
+}
+
+int konverzijaBinarni(int x)
+{
+    if (x == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return(x % 2 + 10 * konverzijaBinarni(x / 2));
+    }
+}
